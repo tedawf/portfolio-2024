4e009100e7c21827780847087a0a646805828bcb
Ang Wei Feng (Ted) <hello@tedawf.com>
2024-04-28 16:50:55 +0800

    add footer

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -2,3 +2,26 @@ export const baseData = {
   title: "tedawf",
   description: "my personal web portfolio",
 };
+
+export const socialData = [
+  {
+    title: "instagram",
+    href: "https://instagram.com/tedawf",
+    icon: "icon--instagram",
+  },
+  {
+    title: "twitter",
+    href: "https://twitter.com/ted_awf",
+    icon: "icon--twitter",
+  },
+  {
+    title: "youtube",
+    href: "https://youtube.com/@tedawf",
+    icon: "icon--youtube",
+  },
+  {
+    title: "github",
+    href: "https://github.com/tedawf",
+    icon: "icon--github",
+  },
+];
